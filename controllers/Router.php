36f96dd5940ec54bc1cc9f8c617971c22f6265d7
Jessy-Domingues <jess4960@gmail.com>
2023-03-31 11:39:27 +0200

    page router OK

--- controllers/Router.php
+++ controllers/Router.php
@@ -12,7 +12,7 @@ class Router{
             });
 
             $url = '';
-
+            //Le controller est inclus selon l'action de l'utilisateur
             if(isset($_GET['url'])){
                 $url = explode('/', filter_var($_GET['url'], 
                 FILTER_SANITIZE_URL));
@@ -20,10 +20,21 @@ class Router{
                 $controller = ucfirst(strtolower($url[0]));
                 $controllerClass = "Controller".$controller;
                 $controllerFile = "Controllers/".$controllerClass.".php";
+
+                if(file_exists($controllerFile)){
+                    require_once($controllerFile);
+                    $this->_ctrl = new $controllerClass($url);
+                } else{
+                    throw new Exception('Page introuvable');
+                }
+            } else{
+                require_once('controllers/ControllerAccueil.php');
+                $this->_ctrl = new ControllerAccueil($url);
             }
         }
         catch(Exception $e){
-
+            $errorMsg = $e->getMessage();
+            require_once('views/viewError.php');
         }
         
     }
